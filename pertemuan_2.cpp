d494a2219af0d5d5709e27e0ebbe76541e6a4cee
zualfarizilubis-ship-it <zualfarizilubis@gmail.com>
2026-02-25 22:55:13 +0700

    Add files via upload

---
+++ pertemuan_2.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+using namespace std;
+
+ int main() {
+    system("cls");
+
+     int a;
+     int b;
+    
+   //Assigment operator (+,-,*,/,%)
+     a=10;
+     b=8;
+
+//     //Arethmetic operator (+,-,*,/,%)
+//     int tambah = a + b;
+//     int kurang = a - b;
+//     int kali = a * b;
+//     int bagi = a / b;
+//     float mod = a % b;
+
+//     cout << "Hasil Penjumlahan : " << tambah << endl;
+//     cout << "Hasil Pengurangan : " << kurang << endl;  
+//     cout << "Hasil Perkalian : " << kali << endl;
+//     cout << "Hasil Pembagian : " << bagi << endl;
+//     cout << "Hasil Modulus : " << mod << endl;
+    
+//lational operator
+//Membandingkan keedua nilai dengan operator ==,!=,>,<,>=,<=
+//jika benar akan menghasilkan nilai 1 (true) dan jika salah akan menghasilkan nilai 0 (false)
+    
+// cout << (a==b) << endl;
+// cout << (a>b) << endl;
+// cout << (a<b) << endl;
+// cout << (a>=b) << endl;
+// cout << (a<=b) << endl; 
+// cout << (a!=b) << endl;
+
+// //and (&&)
+
+// cout << (true && true) << endl;
+// cout << (true && false) << endl;
+// cout << (false && true) << endl;    
+// cout << (false && false) << endl;
+
+// //or (||)
+
+// cout << (true || true) << endl;
+// cout << (true || false) << endl;
+// cout << (false || true) << endl;
+// cout << (false || false) << endl;
+
+// //negasi (!)
+// cout << (!true) << endl;
+// cout << (!false) << endl;
+
+// //bitwise operator (&,|,^,~,<<,>>)
+// cout << (a & b) << endl; //bitwise AND
+// cout << (a | b) << endl; //bitwise OR
+// cout << (a ^ b) << endl; //bitwise XOR
+// cout << (~a) << endl;    //bitwise NOT
+// cout << (a << 1) << endl; //bitwise left shift
+// cout << (a >> 1) << endl; //bitwise right shift
+
+//shorthand
+
+// a += 5; // a = a + 5 = 15
+// cout << a << endl;
+
+// a -= 3; // a = a - 3 = 12
+// cout << a << endl;
+
+// a *= 2; // a = a * 2 = 24
+// cout << a << endl;
+
+// a /= 4; // a = a / 4 = 6
+// cout << a << endl;
+
+// a %= 5; // a = a % 5 = 1
+// cout << a << endl;  
+
+
+// //increment dan decrement
+// //pre increment
+
+// cout << a << endl; // a = a + 1 = 2, tampil 2
+// cout << ++a << endl;   // tampil 2
+// cout << a << endl;   // tampil 1
+
+// //pre decrement
+
+// cout << a << endl; // a = a - 1 = 0, tampil 0
+// cout << --a << endl;   // tampil 0
+// cout << a << endl;   // tampil 0
+
+// //post increment
+// cout << a << endl; // tampil 0, a = a + 1 = 1
+// cout << a++ << endl;   // tampil 0
+// cout << a << endl;   // tampil 1
+
+    return 0;
+}
