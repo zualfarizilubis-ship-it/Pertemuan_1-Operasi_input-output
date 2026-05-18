83e1790d5395518bcace5b51e28224712f625672
zualfarizilubis-ship-it <zualfarizilubis@gmail.com>
2026-05-18 08:45:59 +0700

    Add files via upload

---
+++ pertemuan_7.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    system("cls");
+
+    // int var = 10;
+    // int *ptr;
+
+    // ptr = &var;
+
+    // cout << "ALAMAT dari VARIABEL Var                      --> " << &var << endl;
+    // cout << "ALAMAT YANG DITUNJUK oleh pointer             --> " << ptr << endl;
+    // cout << "NILAI dari ALAMAT YANG DITUNJUK oleh pointer  --> " << *ptr << endl;
+
+
+    //Case dimana pointer not exatcly "Menunjuk" sebuah variable, melainkan menunjuk sebuah nilai (array)"
+//     char *ptr;
+//     ptr = "jakarta";
+
+//     cout << ptr << endl;
+//     cout << *ptr << endl;
+
+// Pointer langsung ke char
+//     char *ptr;
+//     *ptr = 'b';
+
+//     cout << ptr;
+
+// // Ini adalah conoth penggunaan pointer yang salah
+// // pointer to underfined behavior
+//     int *ptr;
+//     *ptr = 10;
+
+//     cout << ptr;
+//     cout << *ptr;
+
+
+// Pointwer to array
+int arr[] = {10, 20, 30, 40, 50, 60};
+int *ptr
+
+
+}
